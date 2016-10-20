0c55ad6e3b620d21e8d25cc404f1d920afaaf104
alejandrogarciasalas <garciasalasalejandro@gmail.com>
2016-10-20 01:35:23 -0700

    first commit

---
+++ tests.py
@@ -0,0 +1,12 @@
+import unittest
+from app import * #usually not good practice
+
+
+# class TestApp(unittest.TestCase):
+	# def test_thing(self):
+		# self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    # unittest.main()
+    print("hello world")
